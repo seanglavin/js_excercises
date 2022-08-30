c6c0df8fc955150b0e5df9245a8c09de46252b7c
seanglavin <skglavin@gmail.com>
2022-08-29 19:17:52 -0600

    update 01 02 03 js exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let repeatString = "";
+    for (i = 0; i < num; i++) {
+        repeatString += string;
+    }
+    return repeatString;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringList = string.split("");
+    stringList.reverse();
+    return myReversedString = stringList.join("");
 };
 
 // Do not edit below this line
